65aea71ab269d8d8edf8da37b2b75fb3e45ee7a8
William Tucker <william.tucker29@gmail.com>
2024-02-12 18:38:01 -0600

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -71,13 +71,30 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+
+  const filePath = path.join(exports.dataDir, id + '.txt');
+
+  fs.exists(filePath, (exists) => {
+    if (!exists) {
+      callback(new Error('Todo item with ID of ${id} does not exist'));
+    } else {
+      fs.writeFile(filePath, text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
+
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.delete = (id, callback) => {
